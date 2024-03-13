403aef13c426601e7704665841650b08a8e636eb
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2024-03-13 15:47:35 +0530

    minor change

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 
 app.get('/profile/:userId', (req,res)=>{
     if(!req.params.userId){
-        res.status(403).send('Need userId!');
+        res.status(404).send('Need userId!');
         return;
     }
     if(req.params.userId==='rajeshj11'){
         return res.send('found')
     }
 
-    return res.status(402).send('not found');
+    return res.status(404).send('not found');
 })
 
 app.listen(4000, ()=>{
